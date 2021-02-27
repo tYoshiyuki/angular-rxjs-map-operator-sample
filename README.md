20a6ae36e67185a2b8df2474b74526c735e4b92e
tYoshiyuki <wasiwasimaru@gmail.com>
2021-02-28 00:58:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-qqye76
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-qqye76)
---
+++ src/app/app.component.ts
@@ -0,0 +1,91 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, VERSION } from "@angular/core";
+import { concatMap, mergeMap, switchMap } from "rxjs/operators";
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+}
+
+export interface Posts {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.mergeMapSample();
+    this.switchMapSample();
+    this.concatMapSample();
+  }
+
+  mergeMapSample(): void {
+    console.log("----- mergeMapのサンプルです -----");
+    this.http
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .pipe(
+        mergeMap(x => x), // 配列を要素に平坦化
+        mergeMap(x => {
+          console.log("mergeMap 投入されました -> " + x.id + " -> " + x.name);
+          return this.http.get<Array<Posts>>(
+            `https://jsonplaceholder.typicode.com/posts?userId=${x.id}`
+          );
+        })
+      )
+      .subscribe(x =>
+        console.log(
+          "mergeMap 処理完了しました -> " + x[0].userId + " -> " + x[0].title
+        )
+      );
+  }
+
+  switchMapSample(): void {
+    console.log("----- switchMapのサンプルです -----");
+    this.http
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .pipe(
+        mergeMap(x => x), // 配列を要素に平坦化
+        switchMap(x => {
+          console.log("switchMap 投入されました -> " + x.id + " -> " + x.name);
+          return this.http.get<Array<Posts>>(
+            `https://jsonplaceholder.typicode.com/posts?userId=${x.id}`
+          );
+        })
+      )
+      .subscribe(x =>
+        console.log(
+          "switchMap 処理完了しました -> " + x[0].userId + " -> " + x[0].title
+        )
+      );
+  }
+
+  concatMapSample(): void {
+    console.log("----- concatMapのサンプルです -----");
+    this.http
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .pipe(
+        mergeMap(x => x), // 配列を要素に平坦化
+        concatMap(x => {
+          console.log("concatMap 投入されました -> " + x.id + " -> " + x.name);
+          return this.http.get<Array<Posts>>(
+            `https://jsonplaceholder.typicode.com/posts?userId=${x.id}`
+          );
+        })
+      )
+      .subscribe(x =>
+        console.log(
+          "concatMap 処理完了しました -> " + x[0].userId + " -> " + x[0].title
+        )
+      );
+  }
+}
